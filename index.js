b1986f4b9be86403da73dd72969d78213051da86
relief527 <78325673+relief527@users.noreply.github.com>
2022-11-24 07:16:56 +0000

    11.24

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/',(req,res)=>{
     res.send('Hello Internet of Things.')
 })
 
+// 传感器列表数组
 const sensors = [
     {id:1,name:'温度传感器'},
     {id:2,name:'湿度传感器'},
@@ -17,22 +18,23 @@ const sensors = [
     {id:6,name:'扭矩传感器'},
 ]
 
+// 获取所有的传感器信息
 app.get('/api/sensors',(req,res)=>{
     res.send(sensors)
 })
 
+// 根据传感器id查询某个传感器
 app.get('/api/sensors/:id',(req,res)=>{
     const sensor = sensors.find( c => c.id === parseInt(req.params.id))
-    
     if(!sensor){
         res.status(404).send("The sensor not found")
     }
     res.send(sensor)
-
 })
+
 app.use(express.json());
 // 提交一个传感器信息
-app.post('/api/sensors', (req, res) => {
+app.post('/api/sensors', (req, res) =>{
 //在提交前，对传感器名进行校验，若传感器名为空或者小于4个字符，则返回
  if (!req.body.name || req.body.name.length < 2) {
  // 400 Bad Request
@@ -48,3 +50,40 @@ app.post('/api/sensors', (req, res) => {
   sensors.push(sensor);
   res.send(sensor);
 });
+
+// 根据传感器id更新某个传感器
+app.put('/api/sensors/:id', (req, res) =>{
+    // Look up the sensor
+    // If not existing, return 404
+    const sensor = sensors.find(c => c.id === parseInt(req.params.id));
+    if(!sensor) { // 404
+        return res.status(404).send('The sensor with the given ID was not found!');
+    }
+    // Validate
+    // If invalid, return 400 - Bad request
+    if(!req.body.name || req.body.name.length < 2){
+    // 400 Bad Request
+       res.status(400).send('Name is required and should be minimum 4 characters.');
+       return;
+    }
+    // Update sensor
+    // Return the updated sensor
+    sensor.name = req.body.name;
+    res.send(sensor);
+});
+    
+// 根据传感器id删除某个传感器
+app.delete('/api/sensors/:id', (req, res) =>{
+    // Look up the sensor
+    // Not existing, return 404
+    const sensor = sensors.find(c => c.id === parseInt(req.params.id));
+    if(!sensor){ // 404
+        return res.status(404).send('The sensor with the given ID was not found!');
+    }
+
+    // Delete
+    const index = sensors.indexOf(sensor);
+    sensors.splice(index, 1);
+    // Return the same sensor
+    res.send(sensor);
+  });
